f3b339ae42f94747d1bbc1c739f373a8f1eeb8af
Bijon Miles <bijonmiles01@gmail.com>
2018-09-13 16:20:12 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,6 +6,14 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
+  birthday_kids = {
+   "Timmy" => 9, 
+   "Sarah" => 6, 
+   "Amanda" => 27
+ }
+ birthday_kids["Timmy"]
+ birthday_kids["Sarah"]
+ birthday_kids["Amanda"]
 end
 
 
